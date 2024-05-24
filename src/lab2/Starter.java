8eca5eb4a5ecbea737644a0b5c94c0203b0041b0
Illia Kruhliak <mr.kruglik1717@gmail.com>
2024-05-24 20:18:04 +0300

    task 5 redone

--- src/lab2/Starter.java
+++ src/lab2/Starter.java
@@ -1,11 +1,34 @@
 package lab2;
 
 public class Starter {
-    public static void main(String[] args) {
-        LineWriter writer1 = new LineWriter("|");
-        LineWriter writer2 = new LineWriter("-");
 
-        writer1.start();
-        writer2.start();
+    public static void main(String[] args) {
+        Starter printer = new Starter();
+        Thread thread1 = new Thread(new LineWriter(printer, '-'));
+        Thread thread2 = new Thread(new LineWriter(printer, '|'));
+
+        thread1.start();
+        thread2.start();
+    }
+
+    public void print(char symbol) {
+        for (int i = 0; i < 100; i++) {
+            System.out.print(symbol);
+        }
+    }
+}
+
+class LineWriter implements Runnable {
+    private final Starter printer;
+    private final char symbol;
+
+    public LineWriter(Starter printer, char symbol) {
+        this.printer = printer;
+        this.symbol = symbol;
+    }
+
+    @Override
+    public void run() {
+        printer.print(symbol);
     }
 }
