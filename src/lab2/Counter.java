f0c3e1fdab9f654bed7c5f92c67ec7d9a6ce3875
Illia Kruhliak <mr.kruglik1717@gmail.com>
2024-05-26 10:58:25 +0300

    tast8

--- src/lab2/Counter.java
+++ src/lab2/Counter.java
@@ -14,8 +14,6 @@ public class Counter {
 
     @Override
     public String toString() {
-        return "Counter{" +
-                "value=" + value +
-                '}';
+        return "Counter output: " + value;
     }
 }
